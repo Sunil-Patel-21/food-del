70355b85692562ba38d22314edca8a1a7d122bb5
Sunil-Patel-21 <patelsunil60273@gmail.com>
2025-11-27 14:19:37 +0530

    fix bugs

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,3 +1,5 @@
+
+
 import { deleteUser, getAllUsers, loginUser, registerUser } from "../controllers/userControllers.js";
 import express from "express";
 
@@ -7,6 +9,6 @@ userRouter.post("/register",registerUser);
 userRouter.post("/login",loginUser);
 userRouter.get("/getAllUsers",getAllUsers)
 userRouter.delete("/deleteUser/:id",deleteUser)
-router.put("/update-food/:id", updateFood);
+
 
 export default userRouter;
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,19 +3,21 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header">
-      {/* ----- main div  */}
+      {/* Background Video */}
+      <video className="header-video" autoPlay muted loop playsInline>
+        <source src="/FoodDelHeroVideo.mp4" type="video/mp4" />
+      </video>
 
+      {/* ----- main div  */}
       <div className="header-contents">
         <h2>Order Your favorite food here.</h2>
         <p>
-          Choose from diverse menu featuring a delectable arry of dish crafts
+          Choose from diverse menu featuring a delectable array of dishes crafted
           with finest ingredients and taste. Our mission is to satisfy your
-          cravings and elevate your dining experience.{" "}
+          cravings and elevate your dining experience.
         </p>
         <button>View Menu</button>
       </div>
-
-      {/* --------------------Main Div  */}
     </div>
   );
 }
